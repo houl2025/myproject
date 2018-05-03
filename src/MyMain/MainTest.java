3e8eebb1fa61445d53ccf134b29f54b3512a2be7
houl2025 <1023797244@qq.com>
2018-05-03 12:43:48 +0800

    更新2018-5-4

--- src/MyMain/MainTest.java
+++ src/MyMain/MainTest.java
@@ -3,8 +3,10 @@ package MyMain;
 public class MainTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        System.out.println("h_w");
+		    // TODO Auto-generated method stub
+          System.out.println("h_w");
+          mytest1 t1=new mytest1();
+          t1.show();      
 	}
 
 }
---
+++ src/MyMain/mytest1.java
@@ -0,0 +1,8 @@
+package MyMain;
+
+public class mytest1 {
+    int i;
+    public void show(){
+    	System.out.println(i);
+    }
+}
