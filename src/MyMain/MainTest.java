4c8276deb2a292f463e0a2d4243987e344f144ba
li2025 <h18713505074@163.com>
2018-05-06 23:10:14 +0800

    "mytest2"

--- src/MyMain/MainTest.java
+++ src/MyMain/MainTest.java
@@ -6,7 +6,9 @@ public class MainTest {
 		    // TODO Auto-generated method stub
           System.out.println("h_w");
           mytest1 t1=new mytest1();
-          t1.show();      
+          t1.show();   
+          mytest2 t2=new mytest2();
+          t2.show();     
 	}
 
 }
